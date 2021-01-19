30b5f7f6c9f39c950763dbcedc1d5341682706d9
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-01-19 21:10:27 +0200

    after 1-st day

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,52 @@
+class Twitter {
+  constructor({ listElem }) {
+    this.tweets = new Posts();
+    this.elements = {
+      listElem: document.querySelector(listElem),
+    };
+  }
+  renderPosts() {}
+  showUserPosts() {}
+  showLikesPosts() {}
+  showAllPost() {}
+  openModal() {}
+}
 
+class Posts {
+  constructor({ posts = [] } = {}) {
+    this.posts = posts;
+  }
+  addPost(tweet) {
+    const post = new Posts(tweet);
+    this.tweets.push(post);
+  }
+  deletePost(id) {}
+  likePost(id) {}
+}
+
+class Post {
+  constructor(param) {
+    this.id = param.id;
+    this.userName = param.userName;
+    this.nickname = param.nickname;
+    this.postDate = param.postDate;
+    this.text = param.text;
+    this.img = param.img;
+    this.likes = param.likes;
+    this.liked = false;
+  }
+  changeLike() {
+    this.like = !this.liked;
+    if (this.liked === true) {
+      this.liked++;
+    } else {
+      this.liked--;
+    }
+  }
+}
+
+const twitter = new Twitter({
+  listElem: ".tweet-list",
+});
+
+console.log(twitter);
